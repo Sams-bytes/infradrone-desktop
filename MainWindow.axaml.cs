d5c3a83b5988436650661ed21f0ec3b7957a3547
Ehetasham Tahir <shamiss2002@gmail.com>
2026-06-27 22:30:42 +0200

    Add main window layout with sidebar navigation and dark theme

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace InfraDroneDesktop;
 
@@ -7,5 +8,40 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void OnFlightView(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Load flight view
+    }
+
+    private void OnMissionView(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Load mission view
+    }
+
+    private void OnPreflightView(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Load preflight view
+    }
+
+    private void OnWeatherView(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Load weather view
+    }
+
+    private void OnParamsView(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Load params view
+    }
+
+    private void OnAuditView(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Load audit view
+    }
+
+    private void OnConnect(object? sender, RoutedEventArgs e)
+    {
+        // TODO: Connect to MAVLink
     }
 }
